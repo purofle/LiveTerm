9ee0bb666c092caae2b5b4594357e559f10f78c8
purofle <purofle@gmail.com>
2024-08-08 22:50:46 +0800

    refactor: change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base">
+          <main className="bg-light-background dark:bg-dark-background w-full h-full p-2">
+            {children}
+          </main>
+        </div>
+      </body>
     </html>
   )
 }
